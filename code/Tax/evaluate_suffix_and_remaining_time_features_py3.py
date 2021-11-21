fbc9a9f6fad6bdb670d94e614bd81a2b3c31c6d9
Renato Marinho Alves <renato.mar.alves@gmail.com>
2021-11-21 16:33:08 -0300

    Adding results and correcting codes

---
+++ code/Tax/evaluate_suffix_and_remaining_time_features_py3.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from __future__ import division
+from keras.models import load_model
+import csv
+import copy
+import sys
+import getopt
+import numpy as np
+import distance
+# from itertools import zip
+from jellyfish._jellyfish import damerau_levenshtein_distance
+# import unicodecsv
+from sklearn import metrics
+from math import sqrt
+import time
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from collections import Counter
+import os
+import pandas as pd
+begin_time = datetime.now()
+
+
+# In[ ]:
+
+
+def load_data(eventlog, path):
+    #return pd.read_csv('../../../work/renato.alves/datasets/'+path+'/%s' % eventlog, sep="|", error_bad_lines=False).values
+    return pd.read_csv('../../dataset/'+path+'/%s' % eventlog, sep=",", error_bad_lines=False).values
+
+
+# In[ ]:
+
+
+def get_divisor(timeseqs):
+    return np.mean([item for sublist in timeseqs for item in sublist])
+
+
+# In[ ]:
+
+
+def create_result_folder(name, dirc):
+    i = 1
+    
+    path_dir = "../../Results/output_files/results/"+dirc
+    if os.path.isdir(path_dir) == False:
+        try:
+            os.mkdir(path_dir)
+        except OSError:
+            print ("Creation of the directory %s failed" % path_dir)
+        else:
+            print ("Successfully created the directory %s " % path_dir)
+    
+    for i in range(100):
+        new_name = name + "_v" + str(i)
+        path_name = path_dir + "/" + new_name
+        if os.path.isdir(path_name) == False:
+            try:
+                os.mkdir(path_name)
+            except OSError:
+                continue
+            else:
+                print ("Successfully created the directory %s " % path_name)
+                break
+                
+    return new_name
+
+
+# In[ ]:
+
+
+def encode(sentence, times, times3, feat, maxlen, num_add_feats, chars, char_indices, divisor, divisor2):
+    num_features = len(chars)+5+num_add_feats+1
+    X = np.zeros((1, maxlen, num_features), dtype=np.float32)
+    leftpad = maxlen-len(sentence)
+    times2 = np.cumsum(times)
+    for t, char in enumerate(sentence):
+        midnight = times3[t].replace(hour=0, minute=0, second=0, microsecond=0)
+        timesincemidnight = times3[t]-midnight
+        multiset_abstraction = Counter(sentence[:t+1])
+        for c in chars:
+            if c==char:
+                X[0, t+leftpad, char_indices[c]] = 1
+        X[0, t+leftpad, len(chars)] = t+1
+        X[0, t+leftpad, len(chars)+1] = times[t]/divisor
+        X[0, t+leftpad, len(chars)+2] = times2[t]/divisor2
+        X[0, t+leftpad, len(chars)+3] = timesincemidnight.seconds/86400
+        X[0, t+leftpad, len(chars)+4] = times3[t].weekday()/7
+        if num_add_feats > 0:
+            for f in range(num_add_feats):
+                X[0, t+leftpad, len(chars)+f+5] = feat[f]
+    return X
+
+
+# In[ ]:
+
+
+def getSymbol(predictions, target_indices_char):
+    maxPrediction = 0
+    symbol = ''
+    i = 0;
+    for prediction in predictions:
+        if(prediction>=maxPrediction):
+            maxPrediction = prediction
+            symbol = target_indices_char[i]
+        i += 1
+    return symbol
+
+
+# In[ ]:
+
+
+def main(argv = None):
+    
+    if argv is None:
+        argv = sys.argv
+
+    inputfile = ""
+    directory = ""
+    model_path=""
+    num_add_feats = 0
+    
+    try:
+        opts, args = getopt.getopt(argv, "hi:d:m:n:")
+    except getopt.GetoptError:
+        print(os.path.basename(__file__),
+              "-i <input_file> -d <directory> -m <model path> -n <num_features>")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(os.path.basename(__file__),
+                  "-i <input_file> -o <output_file> -m <mode pathl> -n <num_features>")
+            sys.exit()
+        elif opt == "-i":
+            inputfile = arg
+        elif opt == "-d":
+            directory = arg
+        elif opt == "-m":
+            model_path = arg
+        elif opt =="-n":
+            num_add_feats = int(arg)
+    
+    begin_time = datetime.now()
+
+    lastcase = ''
+    line = ''
+    firstLine = True
+    lines = []
+    caseids = []
+    timeseqs = []  # relative time since previous event
+    timeseqs2 = [] # relative time since case start
+    timeseqs3 = [] # absolute time of previous event
+    add_feats = []
+    times = []
+    times2 = []
+    times3 = []
+    add_feat = []
+    numlines = 0
+    casestarttime = None
+    lasteventtime = None
+
+    ascii_offset = 161
+    
+    
+    spamreader = load_data(inputfile, directory)
+    
+    for row in spamreader:
+        t = time.strptime(row[2], "%Y-%m-%d %H:%M:%S")
+        if row[0]!=lastcase:
+            caseids.append(row[0])
+            casestarttime = t
+            lasteventtime = t
+            lastcase = row[0]
+            if not firstLine:        
+                lines.append(line)
+                timeseqs.append(times)
+                timeseqs2.append(times2)
+                timeseqs3.append(times3)
+                add_feats.append(list(add_feat))
+            line = ''
+            times = []
+            times2 = []
+            times3 = []
+            add_feat = row[3:]
+            numlines+=1
+        line+=chr(int(row[1])+ascii_offset)
+        timesincelastevent = datetime.fromtimestamp(time.mktime(t))-datetime.fromtimestamp(time.mktime(lasteventtime))
+        timesincecasestart = datetime.fromtimestamp(time.mktime(t))-datetime.fromtimestamp(time.mktime(casestarttime))
+        midnight = datetime.fromtimestamp(time.mktime(t)).replace(hour=0, minute=0, second=0, microsecond=0)
+        timesincemidnight = datetime.fromtimestamp(time.mktime(t))-midnight
+        timediff = 86400 * timesincelastevent.days + timesincelastevent.seconds
+        timediff2 = 86400 * timesincecasestart.days + timesincecasestart.seconds
+        times.append(timediff)
+        times2.append(timediff2)
+        times3.append(datetime.fromtimestamp(time.mktime(t)))
+        # feat = row[3]
+        # servicetype.append(feat)
+        lasteventtime = t
+        firstLine = False
+
+    # add last case
+    lines.append(line)
+    timeseqs.append(times)
+    timeseqs2.append(times2)
+    timeseqs3.append(times3)
+    add_feats.append(add_feat)
+    numlines+=1
+    
+    divisor = get_divisor(timeseqs) #average time between events
+    print('divisor: {}'.format(divisor))
+    divisor2 = get_divisor(timeseqs2) #average time between current and first events
+    print('divisor2: {}'.format(divisor2))
+    divisor3 = np.mean(list(map(lambda x: np.mean(list(map(lambda y: x[len(x)-1]-y, x))), timeseqs2)))
+    print('divisor3: {}'.format(divisor3))
+    
+    #%%
+    elems_per_fold = int(round(numlines/3))
+    fold1 = lines[:elems_per_fold]
+    fold1_c = caseids[:elems_per_fold]
+    fold1_t = timeseqs[:elems_per_fold]
+    fold1_t2 = timeseqs2[:elems_per_fold]
+
+    fold2 = lines[elems_per_fold:2*elems_per_fold]
+    fold2_c = caseids[elems_per_fold:2*elems_per_fold]
+    fold2_t = timeseqs[elems_per_fold:2*elems_per_fold]
+    fold2_t2 = timeseqs2[elems_per_fold:2*elems_per_fold]
+    
+    #%%
+    fold3 = lines[2*elems_per_fold:]
+    fold3_c = caseids[2*elems_per_fold:]
+    fold3_t = timeseqs[2*elems_per_fold:]
+    fold3_t2 = timeseqs2[2*elems_per_fold:]
+    fold3_t3 = timeseqs3[2*elems_per_fold:]
+    fold3_ft = add_feats[2*elems_per_fold:]
+
+    lines = fold1 + fold2
+    caseids = fold1_c + fold2_c
+    lines_t = fold1_t + fold2_t
+    lines_t2 = fold1_t2 + fold2_t2
+    
+    step = 1
+    sentences = []
+    softness = 0
+    next_chars = []
+    lines = list(map(lambda x: x+'!',lines))
+    maxlen = max(map(lambda x: len(x),lines))
+
+    chars = list(map(lambda x : set(x),lines))
+    chars = list(set().union(*chars))
+    chars.sort()
+    target_chars = copy.copy(chars)
+    chars.remove('!')
+    print('total chars: {}, target chars: {}'.format(len(chars), len(target_chars)))
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
+    target_char_indices = dict((c, i) for i, c in enumerate(target_chars))
+    target_indices_char = dict((i, c) for i, c in enumerate(target_chars))
+    print(indices_char)
+
+    lines = fold3
+    caseids = fold3_c
+    lines_t = fold3_t
+    lines_t2 = fold3_t2
+    lines_t3 = fold3_t3
+    lines_ft = fold3_ft
+    
+    #set parameters
+    predict_size = maxlen
+    # load model, set this to the model generated by train.py
+    model = load_model('../../Results/output_files/models/'+directory+'/'+model_path)
+    
+    one_ahead_gt = []
+    one_ahead_pred = []
+
+    two_ahead_gt = []
+    two_ahead_pred = []
+
+    three_ahead_gt = []
+    three_ahead_pred = []
+    
+    result_folder = ""
+    if num_add_feats == 0:
+        result_folder = create_result_folder("suffix_pred_nofeat",directory)
+    else:
+        result_folder = create_result_folder("suffix_pred_"+num_add_feats+"_feats", directory)
+    
+    # make predictions
+    with open('../../Results/output_files/results/'+directory+'/'+result_folder+'/suffix_and_remaining_time_%s' % inputfile, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["CaseID", "Prefix length", "Groud truth", "Predicted", "Levenshtein", "Damerau", "Jaccard", "Ground truth times", "Predicted times", "RMSE", "MAE", "MAE_days"])
+        for prefix_size in range(2,maxlen):
+            # print(prefix_size)
+            print('Prevendo para prefixo: '+str(prefix_size)+'...')
+            for line, caseid, times, times2, times3, ft in zip(lines, caseids, lines_t, lines_t2, lines_t3, lines_ft):
+                times.append(0)
+                cropped_line = ''.join(line[:prefix_size])
+                cropped_times = times[:prefix_size]
+                cropped_times2 = times2[:prefix_size]
+                cropped_times3 = times3[:prefix_size]
+                if len(times2)<prefix_size:
+                    continue # make no prediction for this case, since this case has ended already
+                ground_truth = ''.join(line[prefix_size:prefix_size+predict_size])
+                ground_truth_t = times2[prefix_size-1]
+                case_end_time = times2[len(times2)-1]
+                ground_truth_t = case_end_time-ground_truth_t
+                predicted = ''
+                total_predicted_time = 0
+                for i in range(predict_size):
+                    enc = encode(cropped_line, cropped_times, cropped_times3, ft, maxlen, num_add_feats, chars, char_indices, divisor, divisor2)
+                    y = model.predict(enc, verbose=0) # make predictions
+                    # split predictions into seperate activity and time predictions
+                    y_char = y[0][0] 
+                    y_t = y[1][0][0]
+                    prediction = getSymbol(y_char, target_indices_char) # undo one-hot encoding           
+                    cropped_line += prediction
+                    if y_t<0:
+                        y_t=0
+                    cropped_times.append(y_t)
+                    if prediction == '!': # end of case was just predicted, therefore, stop predicting further into the future
+                        one_ahead_pred.append(total_predicted_time)
+                        one_ahead_gt.append(ground_truth_t)
+                        # print('! predicted, end case')
+                        break
+                    y_t = y_t * divisor3
+                    cropped_times3.append(cropped_times3[-1] + timedelta(seconds=y_t))
+                    total_predicted_time = total_predicted_time + y_t
+                    predicted += prediction
+                output = []
+                if len(ground_truth)>0:
+                    output.append(caseid)
+                    output.append(prefix_size)
+                    output.append(ground_truth)
+                    output.append(predicted)
+                    output.append(1 - distance.nlevenshtein(predicted, ground_truth))
+                    dls = 1 - (damerau_levenshtein_distance(predicted, ground_truth) / max(len(predicted),len(ground_truth)))
+                    if dls<0:
+                        dls=0 # we encountered problems with Damerau-Levenshtein Similarity on some linux machines where the default character encoding of the operating system caused it to be negative, this should never be the case
+                    output.append(dls)
+                    output.append(1 - distance.jaccard(predicted, ground_truth))
+                    output.append(ground_truth_t)
+                    output.append(total_predicted_time)
+                    output.append('')
+                    output.append(metrics.mean_absolute_error([ground_truth_t], [total_predicted_time]))
+                    #output.append(metrics.median_absolute_error([ground_truth_t], [total_predicted_time]))
+                    spamwriter.writerow(output)
+
+    print(datetime.now() - begin_time)
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
---
+++ code/Tax/train_addittional_feats_py3.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Library Imports
+from __future__ import print_function, division
+from keras.models import Sequential, Model
+from keras.layers.core import Dense
+from keras.layers.recurrent import LSTM, GRU, SimpleRNN
+from keras.layers import Input
+from keras.utils.data_utils import get_file
+from keras.optimizers import Nadam
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.layers.normalization import BatchNormalization
+from collections import Counter
+import numpy as np
+import random
+import sys
+import os
+import getopt
+import copy
+import csv
+import io
+import time
+import ipywidgets
+import traitlets
+from datetime import datetime
+from math import log
+import pandas as pd
+from ATLSTM_layer import ATLSTM_layer
+
+
+# In[ ]:
+
+
+def load_data(eventlog, path, sep="|"):
+    return pd.read_csv('../../../dataset/'+path+'/%s' % eventlog, sep=sep, error_bad_lines=False).values
+
+
+# # In[ ]:
+
+
+def get_divisor(timeseqs):
+    return np.mean([item for sublist in timeseqs for item in sublist])
+
+def create_model_folder(name, dirc):
+    i = 1
+    
+    path_dir = "../../../results/output_files/models/"+dirc
+    if os.path.isdir(path_dir) == False:
+        try:
+            os.mkdir(path_dir)
+        except OSError:
+            print ("Creation of the directory %s failed" % path_dir)
+        else:
+            print ("Successfully created the directory %s " % path_dir)
+    
+    for i in range(100):
+        new_name = name + "_v" + str(i)
+        path_name = path_dir + "/" + new_name
+        if os.path.isdir(path_name) == False:
+            try:
+                os.mkdir(path_name)
+            except OSError:
+                continue
+            else:
+                print ("Successfully created the directory %s " % path_name)
+                break
+                
+    return new_name
+# In[ ]:
+
+
+def main(argv = None):
+    if argv is None:
+        argv = sys.argv
+
+    inputfile = ""
+    directory = ""
+    sep=""
+    num_add_feats = 0
+    
+    try:
+        opts, args = getopt.getopt(argv, "hi:d:n:")
+    except getopt.GetoptError:
+        print(os.path.basename(__file__),
+              "-i <input_file> -d <directory> -s <separator> -n <num_add_feats>")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(os.path.basename(__file__),
+                  "-i <input_file> -d <directory> -s <separator> -n <num_add_feats>")
+            sys.exit()
+        elif opt == "-i":
+            inputfile = arg
+        elif opt == "-d":
+            directory = arg
+        elif opt == "-s":
+            sep = arg
+        elif opt == "-n":
+            num_add_feats = int(arg)
+    
+    begin_time = datetime.now()
+    
+    
+    #helper variables
+    lines = [] #these are all the activity seq
+#     timeseqs = [] #time sequences (differences between two events)
+#     timeseqs2 = [] #time sequences (differences between the current and first)
+    lastcase = ''
+    line = ''
+    firstLine = True
+    lines = []
+    timeseqs = []
+    timeseqs2 = []
+    timeseqs3 = []
+    timeseqs4 = []
+    add_feats = []
+    times = []
+    times2 = []
+    times3 = []
+    times4 = []
+    add_feat = -1
+    numlines = 0
+    casestarttime = None
+    lasteventtime = None
+        
+    ascii_offset = 161
+
+    spamreader = load_data(inputfile, directory, sep)
+    
+    for row in spamreader:
+        t = time.strptime(row[2], "%Y-%m-%d %H:%M:%S")
+        if row[0]!=lastcase:
+            casestarttime = t
+            lasteventtime = t
+            lastcase = row[0]
+            if not firstLine:
+                lines.append(line)
+                timeseqs.append(times)
+                timeseqs2.append(times2)
+                timeseqs3.append(times3)
+                timeseqs4.append(times4)
+                add_feats.append(list(add_feat))
+            line = ''
+            times = []
+            times2 = []
+            times3 = []
+            times4 = []
+            add_feat = row[3:]
+            # add_feat = int(row[3])
+            numlines+=1
+        line+=chr(int(row[1])+ascii_offset)
+        timesincelastevent = datetime.fromtimestamp(time.mktime(t))-datetime.fromtimestamp(time.mktime(lasteventtime))
+        timesincecasestart = datetime.fromtimestamp(time.mktime(t))-datetime.fromtimestamp(time.mktime(casestarttime))
+        midnight = datetime.fromtimestamp(time.mktime(t)).replace(hour=0, minute=0, second=0, microsecond=0)
+        timesincemidnight = datetime.fromtimestamp(time.mktime(t))-midnight
+        timediff = 86400 * timesincelastevent.days + timesincelastevent.seconds
+        timediff2 = 86400 * timesincecasestart.days + timesincecasestart.seconds
+        timediff3 = timesincemidnight.seconds #this leaves only time even occured after midnight
+        timediff4 = datetime.fromtimestamp(time.mktime(t)).weekday() #day of the week
+        times.append(timediff)
+        times2.append(timediff2)
+        times3.append(timediff3)
+        times4.append(timediff4)
+        # add_feats.append(add_feat)
+        lasteventtime = t
+        firstLine = False
+
+    lines.append(line)
+    timeseqs.append(times)
+    timeseqs2.append(times2)
+    timeseqs3.append(times3)
+    timeseqs4.append(times4)
+    add_feats.append(add_feat)
+    numlines+=1
+
+    divisor = get_divisor(timeseqs) #average time between events
+    print('divisor: {}'.format(divisor))
+    divisor2 = get_divisor(timeseqs2) #average time between current and first events
+    print('divisor2: {}'.format(divisor2))
+
+    elems_per_fold = int(round(numlines/3))
+    fold1 = lines[:elems_per_fold]
+    fold1_t = timeseqs[:elems_per_fold]
+    fold1_t2 = timeseqs2[:elems_per_fold]
+    fold1_t3 = timeseqs3[:elems_per_fold]
+    fold1_t4 = timeseqs4[:elems_per_fold]
+    fold1_ft = add_feats[:elems_per_fold]
+    with open('../../../results/output_files/folds/fold1.csv', 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row, timeseq in zip(fold1, fold1_t):
+            spamwriter.writerow([s +'#{}'.format(t) for s, t in zip(row, timeseq)])
+
+    fold2 = lines[elems_per_fold:2*elems_per_fold]
+    fold2_t = timeseqs[elems_per_fold:2*elems_per_fold]
+    fold2_t2 = timeseqs2[elems_per_fold:2*elems_per_fold]
+    fold2_t3 = timeseqs3[elems_per_fold:2*elems_per_fold]
+    fold2_t4 = timeseqs4[elems_per_fold:2*elems_per_fold]
+    fold2_ft = add_feats[elems_per_fold:2*elems_per_fold]
+    with open('../../../results/output_files/folds/fold2.csv', 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row, timeseq in zip(fold2, fold2_t):
+            spamwriter.writerow([s +'#{}'.format(t) for s, t in zip(row, timeseq)])
+
+    fold3 = lines[2*elems_per_fold:]
+    fold3_t = timeseqs[2*elems_per_fold:]
+    fold3_t2 = timeseqs2[2*elems_per_fold:]
+    fold3_t3 = timeseqs3[2*elems_per_fold:]
+    fold3_t4 = timeseqs4[2*elems_per_fold:]
+    fold3_ft = add_feats[2*elems_per_fold:]
+    with open('../../../results/output_files/folds/fold3.csv', 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row, timeseq in zip(fold3, fold3_t):
+            spamwriter.writerow([s +'#{}'.format(t) for s, t in zip(row, timeseq)])
+
+    lines = fold1 + fold2
+    lines_t = fold1_t + fold2_t
+    lines_t2 = fold1_t2 + fold2_t2
+    lines_t3 = fold1_t3 + fold2_t3
+    lines_t4 = fold1_t4 + fold2_t4
+    lines_ft = fold1_ft + fold2_ft
+
+    step = 1
+    sentences = []
+    softness = 0
+    next_chars = []
+    lines = list(map(lambda x: x+'!',lines)) #put delimiter symbol
+    maxlen = max(list(map(lambda x: len(x),lines))) #find maximum line size
+
+    # next lines here to get all possible characters for events and annotate them with numbers
+    chars = list(map(lambda x: set(x),lines))
+    chars = list(set().union(*chars))
+    chars.sort()
+    target_chars = copy.copy(chars)
+    chars.remove('!')
+    print('total chars: {}, target chars: {}'.format(len(chars), len(target_chars)))
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
+    target_char_indices = dict((c, i) for i, c in enumerate(target_chars))
+    target_indices_char = dict((i, c) for i, c in enumerate(target_chars))
+    print(indices_char)
+
+    sentences_t = []
+    sentences_t2 = []
+    sentences_t3 = []
+    sentences_t4 = []
+    sentences_ft = []
+    next_chars_t = []
+    next_chars_t2 = []
+    next_chars_t3 = []
+    next_chars_t4 = []
+    next_chars_ft = []
+    for line, line_t, line_t2, line_t3, line_t4, line_ft in zip(lines, lines_t, lines_t2, lines_t3, lines_t4, lines_ft):
+        for i in range(0, len(line), step):
+            if i==0:
+                continue
+
+            #we add iteratively, first symbol of the line, then two first, three...
+
+            sentences.append(line[0: i])
+            sentences_t.append(line_t[0:i])
+            sentences_t2.append(line_t2[0:i])
+            sentences_t3.append(line_t3[0:i])
+            sentences_t4.append(line_t4[0:i])
+            sentences_ft.append(line_ft)
+            next_chars.append(line[i])
+            if i==len(line)-1: # special case to deal time of end character
+                next_chars_t.append(0)
+                next_chars_t2.append(0)
+                next_chars_t3.append(0)
+                next_chars_t4.append(0)
+            else:
+                next_chars_t.append(line_t[i])
+                next_chars_t2.append(line_t2[i])
+                next_chars_t3.append(line_t3[i])
+                next_chars_t4.append(line_t4[i])
+            next_chars_ft.append(line_ft)
+    print('nb sequences:', len(sentences))
+
+    print('Vectorization...')
+    num_features = len(chars)+5+num_add_feats+1
+    print('num features: {}'.format(num_features))
+    X = np.zeros((len(sentences), maxlen, num_features), dtype=np.float32)
+    y_a = np.zeros((len(sentences), len(target_chars)), dtype=np.float32)
+    y_t = np.zeros((len(sentences)), dtype=np.float32)
+    for i, sentence in enumerate(sentences):
+        leftpad = maxlen-len(sentence)
+        next_t = next_chars_t[i]
+        sentence_t = sentences_t[i]
+        sentence_t2 = sentences_t2[i]
+        sentence_t3 = sentences_t3[i]
+        sentence_t4 = sentences_t4[i]
+        # sentence_ft = sentences_ft[i][0]
+        # sentence_ft2 = sentences_ft[i][1]
+        # sentence_ft3 = sentences_ft[i][2]
+        for t, char in enumerate(sentence):
+            multiset_abstraction = Counter(sentence[:t+1])
+            for c in chars:
+                if c==char: #this will encode present events to the right places
+                    X[i, t+leftpad, char_indices[c]] = 1
+            X[i, t+leftpad, len(chars)] = t+1
+            X[i, t+leftpad, len(chars)+1] = sentence_t[t]/divisor
+            X[i, t+leftpad, len(chars)+2] = sentence_t2[t]/divisor2
+            X[i, t+leftpad, len(chars)+3] = sentence_t3[t]/86400
+            X[i, t+leftpad, len(chars)+4] = sentence_t4[t]/7
+            # X[i, t+leftpad, len(chars)+5] = next_chars_t[t]
+            if num_add_feats > 0:
+                for f in range(num_add_feats):
+                    X[i, t+leftpad, len(chars)+f+5] = sentences_ft[i][f]
+            # X[i, t+leftpad, len(chars)+6] = sentence_ft
+            # X[i, t+leftpad, len(chars)+7] = sentence_ft2
+            # X[i, t+leftpad, len(chars)+8] = sentence_ft3
+        for c in target_chars:
+            if c==next_chars[i]:
+                y_a[i, target_char_indices[c]] = 1-softness
+            else:
+                y_a[i, target_char_indices[c]] = softness/(len(target_chars)-1)
+        y_t[i] = next_t/divisor
+        np.set_printoptions(threshold=sys.maxsize)
+
+    # build the model: 
+    print('Build model...')
+    print(X.shape)
+    
+    main_input = Input(shape=(maxlen, num_features), name='main_input')
+    # train a 2-layer LSTM with one shared layer
+    l1 = ATLSTM_layer(128, return_sequences=True)(main_input) # the shared layer
+
+    l2_1 = ATLSTM_layer(128, return_sequences=False)(l1)
+
+    l2_2 = ATLSTM_layer(128, return_sequences=False)(l1)
+
+    d1 = keras.layers.Dropout(.2)(l1)
+    d2_1 = keras.layers.Dropout(.2)(l2_1)
+    d2_2 = keras.layers.Dropout(.2)(l2_2)
+
+    act_output = Dense(len(target_chars), activation='softmax', kernel_initializer='glorot_uniform', name='act_output')(d2_1)
+    time_output = Dense(1, kernel_initializer='glorot_uniform', name='time_output')(d2_2)
+    
+    model = Model(inputs=[main_input], outputs=[act_output, time_output])
+    
+    model_folder = ""
+    if num_add_feats == 0:
+        model_folder = create_model_folder("model_nofeat",directory)
+    else:
+        model_folder = create_model_folder("model_"+num_add_feats+"_feat", directory)
+    
+    opt = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004, clipvalue=3)
+    
+    model.compile(loss={'act_output':'categorical_crossentropy', 'time_output':'mae'}, optimizer=opt)
+    
+    early_stopping = EarlyStopping(monitor='val_loss', patience=42)
+    
+    model_checkpoint = ModelCheckpoint("../../../results/output_files/models/"+directory+"/"+model_folder+'/model_{epoch:02d}-{val_loss:.2f}.h5',
+                                   monitor='val_loss',
+                                   verbose=0,
+                                   save_best_only=True,
+                                   save_weights_only=False,
+                                   mode='auto')
+                                   
+    lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
+    
+    model.fit({'main_input': X}, {'act_output':y_a, 'time_output':y_t},
+          validation_split=0.2,
+          verbose=2,
+          callbacks=[early_stopping, model_checkpoint, lr_reducer],
+          batch_size=maxlen,
+          epochs=200
+          )
+    
+    print(datetime.now() - begin_time)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
